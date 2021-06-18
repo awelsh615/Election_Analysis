b7dacaae1eff634611f92778b2bb733f59f48a90
Amy Welsh <awelsh@davidsonacademy.com>
2021-06-17 20:25:36 -0500

    read_write from csv commit

--- PyPoll.py
+++ PyPoll.py
@@ -5,11 +5,41 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
+# Add our dependencies
+import csv
+import os
+
 #Assign a variable for the file to load and the path.
-file_to_load='Resources/election_results.csv'
+file_to_load=os.path.join("Resources","election_results.csv")
+
+#Assign a variable to save the file to a path 
+file_to_save=os.path.join("analysis","election_analysis.txt")
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
-    #To do: perform analysis
-    print(election_data)
+    
+    #To do: read analyze data here
+    #Read the file object with the reader function.
+    file_reader=csv.reader(election_data)
+
+    #Read and print the header row.
+    headers=next(file_reader)
+    print(headers)
+
+
+    #print each row in the .csv file
+    #for row in file_reader:
+    #    print(row)
+
+    # Print the file object.
+    #print(election_data)
+
+
+
+
+#Using the with statement, write to the text file
+with open(file_to_save,"w") as txt_file:
+    #Write some data to the file.
+    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+
 
